90586f8f496a57665abaa654d17af5656811d4eb
YT <yt@451-col-s-18023.middlebury.edu>
2019-08-02 10:37:31 -0400

    Add message after morning exile phase

--- src/components/ResultOfMorning.js
+++ src/components/ResultOfMorning.js
@@ -1,22 +1,78 @@
 import React from 'react';
 
-const ResultOfMorning = (props) => {
-  let x = props.to_be_exiled[0];
-  let y = props.handleWinningSide();
+export default class ResultOfMorning extends React.Component {
+  constructor(props) {
+    super(props);
+    this.afterWillPhase = this.afterWillPhase.bind(this);
+    this.continueGamePhase = this.continueGamePhase.bind(this);
+    this.state = {
+      phase: 'will'
+    };
+  }
+
+  afterWillPhase () {
+    this.setState({ phase: 'after_will'})
+  }
+
+  continueGamePhase () {
+    this.setState({ phase: 'continue_game'})
+  }
 
 
-  return(
-    <div className='widget'>
-      <p className='widget widget__message'>{x}さん, 最後に遺言をどうぞ｡</p>
-      <div className="button-wrapper">
-        <button
-          className='button-single'
-          onClick={ y < 0 ? props.nightConfirmPhase: props.gameResultPhase}
-        >
-          次へ
-        </button>
-      </div>
-    </div>
-    )
+  render () {
+    if (this.state.phase === 'will') {
+      return (
+        <div className='widget'>
+          <p className='widget widget__message'>{this.props.to_be_exiled[0]}さん, 最後に遺言をどうぞ｡</p>
+          <div className="button-wrapper">
+            <button
+              className='button-single'
+              onClick={this.afterWillPhase}
+            >
+              次へ
+            </button>
+          </div>
+        </div>
+        )
+
+    } else if (this.state.phase === 'after_will') {
+      return (
+        <div>
+          <div className="widget">
+            <div className='widget__message'>人狼だと疑われた{this.props.to_be_exiled[0]}さんは追放されました。</div>
+          </div>
+
+          <div className="button-wrapper">
+            <button
+              className='button-single'
+              onClick={ this.props.handleWinningSide() < 0 ? this.continueGamePhase: this.props.gameResultPhase}
+            >
+              次へ
+            </button>
+          </div>
+        </div>
+        )
+    } else if (this.state.phase === 'continue_game') {
+      return (
+        <div>
+          <div className="widget">
+            <div className="widget__message">
+              夜が来ました。。。
+            </div>
+          </div>
+          <div className="button-wrapper">
+            <button
+              className='button-single'
+              onClick={ this.props.nightConfirmPhase}
+            >
+              次へ
+            </button>
+          </div>
+        </div>
+        )
+    } else {
+      return (<div></div>)
+    }
+  }
 }
-export default ResultOfMorning;
+
