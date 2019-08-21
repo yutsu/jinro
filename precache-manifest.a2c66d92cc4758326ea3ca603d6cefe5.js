0e5aa5d62b56ea958cdf6da03b9227318bdff3fb
YT <yt@451-col-s-18023.lan>
2019-08-20 21:23:43 -0400

    Updates

---
+++ precache-manifest.a2c66d92cc4758326ea3ca603d6cefe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ffbdc7e45bd9b5b19ffe221d2dbb69",
+    "url": "/jinro/index.html"
+  },
+  {
+    "revision": "d368fcadf268b8e6ab33",
+    "url": "/jinro/static/css/2.e95dff61.chunk.css"
+  },
+  {
+    "revision": "70e872c5694eb6af996d",
+    "url": "/jinro/static/css/main.e8189137.chunk.css"
+  },
+  {
+    "revision": "d368fcadf268b8e6ab33",
+    "url": "/jinro/static/js/2.645c79f0.chunk.js"
+  },
+  {
+    "revision": "70e872c5694eb6af996d",
+    "url": "/jinro/static/js/main.fc562bc9.chunk.js"
+  },
+  {
+    "revision": "45b82ef827985129d6a1",
+    "url": "/jinro/static/js/runtime~main.8220ef53.js"
+  }
+]);
